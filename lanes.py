936fdf741c70739caff297d9440aa233e4485d39
agamyo168 <70448818+agamyo168@users.noreply.github.com>
2022-04-16 07:57:21 +0200

    defined region_of_interest and filter_by_color functions

--- lanes.py
+++ lanes.py
@@ -1,3 +1,42 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as pyplot
+
+image = cv2.imread("./test/test_images/straight_lines1.jpg")
+lane_image = np.copy(image)
+
+
+#Since all lane lines are either yellow or white we are going to crop all yellow and white colors from image/video
+def filter_by_color(img):
+    hls = cv2.cvtColor(img,cv2.COLOR_RGB2HLS)
+    #Hue Lightness Saturation
+    white_lower = np.array([0,190,0])
+    white_upper = np.array([255,255,255])
+    yellow_lower = np.array([10,0,90])
+    yellow_upper = np.array([50,255,255])
+    yellowmask = cv2.inRange(hls, yellow_lower, yellow_upper)
+    whitemask = cv2.inRange(hls, white_lower, white_upper)
+    mask = cv2.bitwise_or(yellowmask, whitemask)
+    masked = cv2.bitwise_and(image, image, mask = mask)
+    return masked
+    
+    
+def region_of_interest(img):
+    x = int(img.shape[1])
+    y = int(img.shape[0])
+    #4 rows = 4 points 2 
+    #columns =(x,y)
+    #shape is array of four points
+    #Triangle Shape
+    shape = np.array([[int(0), int(y)], [int(x), int(y)], [int(0.5*x), int(0.6*y)]])
+    mask = np.zeros_like(img) #Creates an array of same size as image but all zeros
+    cv2.fillPoly(mask,pts=[shape],color=255)
+    masked_image = cv2.bitwise_and(img,mask)
+    return masked_image
+
+mask_by_color = filter_by_color(lane_image)
+roi = region_of_interest(mask_by_color)
+
+
+cv2.imshow("masked_image",roi)
+cv2.waitKey(0)
